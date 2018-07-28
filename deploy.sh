6abc97ba4599dcacadef05c72109a27da8f8fd54
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-07-28 14:03:03 +0200

    support deleting files on deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Initialize target with currently deployed files
+git clone --depth 1 --branch=master https://github.com/wololock/wololock.github.io.git .deploy_git
+
+cd .deploy_git
+
+# Remove all files before they get copied from ../public/
+# so git can track files that were removed in the last commit
+find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+
+cd ../
+
+# Run deployment
+hexo deploy
