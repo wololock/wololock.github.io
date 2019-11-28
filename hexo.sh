73623063119968588d4ff2e494db4e3908e02709
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-11-28 13:00:02 +0100

    fixed browsersync from docker container

--- hexo.sh
+++ hexo.sh
@@ -6,4 +6,4 @@ if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
   docker build -t $DOCKER_IMAGE .
 fi
 
-docker run --rm -u $(id -u) -p 4000:4000 -v $(pwd):/home/node/blog $DOCKER_IMAGE hexo "$@"
+docker run --rm -u $(id -u) -p 4000:4000 -p 3001:3001 -p 3000:3000 -v $(pwd):/home/node/blog $DOCKER_IMAGE hexo "$@"
