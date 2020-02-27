ecfa7553b4827675e02fcfb9aefbea599813c626
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-02-27 10:26:32 +0100

    deployment script updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -d "./.deploy_git" ]; then
+  echo "Removing .deploy_git folder..."
+  rm -rf ./.deploy_git
+  echo "Folder removed."
+fi
+
 # Initialize target with currently deployed files
 git clone --depth 1 --branch=master https://github.com/wololock/wololock.github.io.git .deploy_git
 
@@ -10,6 +16,10 @@ cd .deploy_git
 find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 
 cd ../
+
+if [ ! -d "./public" ]; then
+  hexo generate
+fi
 
 # Run deployment
 hexo deploy
