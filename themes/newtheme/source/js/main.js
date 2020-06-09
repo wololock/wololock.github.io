0f847def88fa17a3bf5f4717b4a73c749f5e411a
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-06-09 13:31:52 +0200

    navbar added

---
+++ themes/newtheme/source/js/main.js
@@ -0,0 +1,32 @@
+!(function($) {
+    "use strict";
+
+    // Toggle .header-scrolled class to #header when page is scrolled
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
+        } else {
+            $('#header').removeClass('bg-white navbar-light shadow-sm scrolled');
+        }
+    });
+
+    if ($(window).scrollTop() > 100) {
+        $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
+    }
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+})(jQuery);
