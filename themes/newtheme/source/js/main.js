a8da9ceae03034f6bfa1fc4d8ec85c30d15c2f32
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-06-10 23:43:01 +0200

    template update

--- themes/newtheme/source/js/main.js
+++ themes/newtheme/source/js/main.js
@@ -2,17 +2,20 @@
     "use strict";
 
     // Toggle .header-scrolled class to #header when page is scrolled
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-            $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
-        } else {
-            $('#header').removeClass('bg-white navbar-light shadow-sm scrolled');
-        }
-    });
+    if (!$('#header').hasClass("bg-white navbar-light shadow-sm scrolled")) {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
+            } else {
+                $('#header').removeClass('bg-white navbar-light shadow-sm scrolled');
+            }
+        });
 
-    if ($(window).scrollTop() > 100) {
-        $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
+        if ($(window).scrollTop() > 100) {
+            $('#header').addClass('bg-white navbar-light shadow-sm scrolled');
+        }
     }
+
     // Back to top button
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
