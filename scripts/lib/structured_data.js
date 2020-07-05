5420fe1a49e46e670d83f62e119b2e7295e198a6
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-07-05 11:25:05 +0200

    structured data improved

--- scripts/lib/structured_data.js
+++ scripts/lib/structured_data.js
@@ -5,43 +5,157 @@ module.exports = structured_data;
 var util = require('hexo-util');
 
 function structured_data(page, config) {
-    var data = {
-        '@context': 'http://schema.org',
-        '@type': 'BlogPosting',
-        'mainEntityOfPage': {
-            '@type': 'WebPage',
-            '@id': config.url + '/' + page.path
-        },
-        'headline': page.title
+
+    const person_id = config.url + '/#/schema/person/wololock';
+    const website_id = config.url + '/#website';
+    const webpage_id = config.url + '/' + page.path + '#webpage';
+    const article_id = config.url + '/' + page.path + '#article';
+    const category = page.categories.toArray()[0];
+
+    const data = {
+        '@context': 'https://schema.org',
+        '@graph': [
+            {
+                '@type': ['Person', 'Organization'],
+                '@id': person_id,
+                'name': config.author,
+                'description': config.bio,
+                'image': {
+                    '@type': 'ImageObject',
+                    '@id': config.url + '/#avatar',
+                    'inLanguage': 'en-US',
+                    'url': config.photo,
+                    'caption': config.author
+                },
+                'logo': {
+                    '@id': config.url + '/#avatar'
+                },
+                'sameAs': [
+                    'https://twitter.com/wololock',
+                    'https://github.com/wololock',
+                    'https://stackoverflow.com/users/2194470/szymon-stepniak',
+                    'https://www.youtube.com/c/eprintstacktrace'
+                ]
+            },
+            {
+                '@type': 'WebSite',
+                '@id': website_id,
+                'url': config.url,
+                'name': config.title,
+                'description': config.description
+            },
+            {
+                '@type': 'WebPage',
+                '@id': webpage_id,
+                'url': config.url + '/' + page.path,
+                'name': page.title,
+                'isPartOf': {
+                    '@id': website_id
+                },
+                'datePublished': page.date,
+                'dateModified': page.updated,
+                'description': util.stripHTML(page.excerpt),
+                'inLanguage': 'en-US',
+                'potentialAction': [
+                    {
+                        '@type': 'ReadAction',
+                        'target': [
+                            config.url + '/' + page.path
+                        ]
+                    }
+                ]
+            },
+            {
+                '@type': 'Article',
+                '@id': article_id,
+                'isPartOf': {
+                    '@id': webpage_id
+                },
+                'author': {
+                    '@id': person_id
+                },
+                'headline': page.title,
+                'datePublished': page.date,
+                'dateModified': page.updated,
+                'mainEntityOfPage': {
+                    '@id': webpage_id
+                },
+                'publisher': {
+                    '@id': person_id
+                },
+                'keywords': page.tags.map(tag => tag.name).join(','),
+                'image': {
+                    '@type': 'ImageObject',
+                    '@id': config.url + '/' + page.path + '#primaryimage',
+                    'url': config.url + page.og_image,
+                    'inLanguage': 'en-US',
+                    'caption': page.title
+                },
+                'inLanguage': 'en-US'
+            },
+            {
+                '@type': 'BreadcrumbList',
+                'itemListElement': [
+                    {
+                        '@type': 'ListItem',
+                        'position': 1,
+                        'item': {
+                            '@id': config.url,
+                            'name': 'Home'
+                        }
+                    },
+                    {
+                        '@type': 'ListItem',
+                        'position': 2,
+                        'item': {
+                            '@id': category.permalink,
+                            'name': category.name
+                        }
+                    },
+                    {
+                        '@type': 'ListItem',
+                        'position': 3,
+                        'item': {
+                            '@id': config.url + '/' + page.path,
+                            'name': page.title
+                        }
+                    }
+                ]
+            }
+        ]
     };
 
-    if (page.og_image !== undefined) {
-        data['image'] = {
-            '@type': 'ImageObject',
-            'url': config.url + page.og_image
-        }
-    }
-
-    data['datePublished'] =  page.date;
-    data['dateModified'] = page.updated;
-    data['author'] = {
-        '@type': 'Person',
-        'name': config.author
-    };
-    data['publisher'] = {
-        '@type': 'Organization',
-        'name': config.title,
-        'logo': {
-            '@type': 'ImageObject',
-            'url': config.url + '/images/site-logo.jpg',
-            'height': 60,
-            'width': 600
-        }
-    };
-    data['description'] = util.stripHTML(page.excerpt);
-    if (page.tags !== undefined) {
-        data['keywords'] = page.tags.map(function(tag) { return tag.name }).join(',');
+    if  (page.structured_data !== undefined) {
+        data['@graph'].push(page.structured_data);
     }
 
     return '<script type="application/ld+json">' + JSON.stringify(data) + '</script>';
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
