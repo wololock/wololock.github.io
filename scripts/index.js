8ed04b63bc153822a57193e60cd6ed93e17049f4
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-08-25 21:49:30 +0200

    hexo 5.1.1 update

--- scripts/index.js
+++ scripts/index.js
@@ -58,3 +58,13 @@ function paginated_url(path, pagedir, page) {
 
     return basePath;
 }
+
+const chunk = (input, size) => {
+    return input.reduce((arr, item, idx) => {
+        return idx % size === 0
+            ? [...arr, [item]]
+            : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
+    }, []);
+};
+
+hexo.extend.helper.register("chunk", chunk);
